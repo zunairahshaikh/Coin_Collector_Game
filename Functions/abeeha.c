e76ee7c2bf2f3c467c0b179be134f29bf346c6df
abeeha binte aamer <abeehakhan50@gmail.com>
2024-11-28 23:56:00 +0500

    Update abeeha.c

--- Functions/abeeha.c
+++ Functions/abeeha.c
@@ -24,5 +24,37 @@ void handleCollision(int newX, int newY)
     }
 }
 
+}
+void displayGame()
+{
+    clearScreen();
+    printf("Score: %d  Lives: %d  Time left: %d\n\n",
+           score, lives, GAME_DURATION - (int)(time(NULL) - startTime));
 
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            switch (grid[i][j])
+            {
+            case PLAYER:
+                printf("🦊 ");
+                break;
+            case COIN:
+                printf("🟡 ");
+                break;
+            case OBSTACLE:
+                printf("🔺 ");
+                break;
+            case EMPTY:
+                printf(" . ");
+                break;
+            default:
+                printf("%c ", grid[i][j]); // fallback for unexpected characters
+            }
+        }
+        printf("\n");
+    }
+    printf("\nUse WASD to move, Q to quit\n");
+}
 
